42541c1a24ef33c931ea74e9dec490b1fcf0063e
23vha <60228678+23vha@users.noreply.github.com>
2020-01-30 11:28:53 -0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+	//Finish this constructor
+    }
+
+    public int getAge() {
+	//finish this getter
+	return 0;
+    }
+
+    public String getName() {
+	//finish this getter
+	return "";
+    }
+
+    public void setName() {
+	//Fix and finish this setter.
+	
+    }
+
+    public void setAge() {
+	//Fix and finish this setter.
+    }
+    
+    
+}
